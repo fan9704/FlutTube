1463464d87ed3bd5c310b8cacdb0afa4addfc0f1
fan9704 <cxz123499@gmail.com>
2022-03-15 13:09:34 +0800

    Change main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:bloc/bloc.dart';
+
 import 'splash_page.dart';
+import 'simple_bloc_delegate.dart';
+import 'firebase/user_repository.dart';
+import 'authentication_bloc/bloc.dart';
+import 'login/login_page.dart';
 
-void main() => runApp(MyApp());
-
+//void main() => runApp(MyApp());
+void main(){
+  BlocSupervisor.delegate = SimpleBlocDelegate();
+  runApp(MyApp());
+}
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
